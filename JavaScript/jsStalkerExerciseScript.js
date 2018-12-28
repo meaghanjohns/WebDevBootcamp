6a4a8c729bba2770d57071402f32ede73ddd8d6c
Meaghan Johns <15947575+meaghanjohns@users.noreply.github.com>
2018-12-27 23:22:44 -0500

    Add files via upload

---
+++ JavaScript/jsStalkerExerciseScript.js
@@ -0,0 +1,5 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var userAge = prompt("What is your age?");
+console.log("Hello. Your full name is " + firstName + " " + lastName + ".");
+console.log("You are " + userAge + " years old.");
